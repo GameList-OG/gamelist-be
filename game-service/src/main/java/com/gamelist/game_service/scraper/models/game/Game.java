03822e0e229079bdea554bcb90a541f7710e6456
Andrew Caruso <yzma@users.noreply.github.com>
2025-08-01 14:25:37 -0400

    Added PlayerPerspective model, updated Game relationship

--- game-service/src/main/java/com/gamelist/game_service/scraper/models/game/Game.java
+++ game-service/src/main/java/com/gamelist/game_service/scraper/models/game/Game.java
@@ -4,6 +4,7 @@ package com.gamelist.game_service.scraper.models.game;
 
 import com.gamelist.game_service.scraper.models.Genre;
 import com.gamelist.game_service.scraper.models.Keyword;
+import com.gamelist.game_service.scraper.models.PlayerPerspective;
 import com.gamelist.game_service.scraper.models.Theme;
 import com.gamelist.game_service.scraper.models.age_rating.AgeRating;
 import com.gamelist.game_service.scraper.models.artwork.Artwork;
@@ -89,6 +90,16 @@ public class Game {
             inverseJoinColumns = @JoinColumn(name = "keyword_id")
     )
     private Set<Keyword> keywords;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "game_player_perspectives",
+            joinColumns = @JoinColumn(name = "game_id"),
+            inverseJoinColumns = @JoinColumn(name = "player_perspective_id")
+    )
+    private Set<PlayerPerspective> playerPerspectives;
 
     @Column(name = "created_at")
     private Instant createdAt;
