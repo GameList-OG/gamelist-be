94c0e3071886608a2ee4399fbefa7dc2cba42453
Andrew Caruso <yzma@users.noreply.github.com>
2025-08-02 17:04:26 -0400

    Added Event and NetworkType models

---
+++ game-service/src/main/java/com/gamelist/game_service/scraper/models/NetworkType.java
@@ -0,0 +1,40 @@
+package com.gamelist.game_service.scraper.models;
+
+import com.gamelist.game_service.scraper.models.event.EventNetwork;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.Set;
+import java.util.UUID;
+
+// https://api-docs.igdb.com/#network-type
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "network_types")
+public class NetworkType {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_network_id")
+    private Set<EventNetwork> events;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    private UUID checksum;
+}
